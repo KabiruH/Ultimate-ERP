14aa1ab646a02cc4c4e2e2b4a73a1c928191c4e4
kabiruH <kelkab46@gmail.com>
2024-02-20 10:52:17 +0300

    displaying table

--- backend-ERP/app/controllers/profit_loss_statements_controller.rb
+++ backend-ERP/app/controllers/profit_loss_statements_controller.rb
@@ -8,37 +8,42 @@ class ProfitLossStatementsController < ApplicationController
   end
 
   def create
-    @profit_loss_statement = ProfitLossStatement.new(profit_loss_statement_params)
-
-    if @profit_loss_statement.save
-      redirect_to profit_loss_statements_path, notice: 'Profit Loss Statement was successfully created.'
-    else
-      render :new
-    end
+    binding.pry
+    @revenue = params[:profit_loss_statement][:revenue].to_f
+    @expenses = params[:profit_loss_statement][:expenses].to_f
+    @sales = params[:profit_loss_statement][:sales].to_f
+    @accounts_receivable = params[:profit_loss_statement][:accounts_receivable].to_f
+    @accounts_payable = params[:profit_loss_statement][:accounts_payable].to_f
+  
+    @profit_loss_data = ProfitLossStatement.generate_data(
+      @revenue, @expenses, @sales, @accounts_receivable, @accounts_payable
+    )
+  
+    render :index
   end
 
   def index
-    @profit_loss_statement = ProfitLossStatement.find(params[:id])
+    @profit_loss_statements = ProfitLossStatement.all
     @start_date = params[:start_date]
     @end_date = params[:end_date]
-    @profit_loss_data = @profit_loss_statement.generate_profit_loss_data(@start_date, @end_date)
+    @profit_loss_data = @profit_loss_statements.map(&:generate_profit_loss_data)
   end
 
-  def download_csv
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
-    @profit_loss_statement = ProfitLossStatement.find(params[:id])
-    @profit_loss_data = @profit_loss_statement.generate_profit_loss_data(@start_date, @end_date)
+  # def download_csv
+  #   @start_date = params[:start_date]
+  #   @end_date = params[:end_date]
+  #   @profit_loss_statement = ProfitLossStatement.find(params[:id])
+  #   @profit_loss_data = @profit_loss_statement.generate_profit_loss_data(@start_date, @end_date)
 
-    # Generate CSV data
-    csv_data = generate_csv_data(@profit_loss_data)
+  #   # Generate CSV data
+  #   csv_data = generate_csv_data(@profit_loss_data)
 
-    # Send the CSV data to the browser
-    send_data csv_data,
-              filename: "profit_loss_statement_#{@start_date}_to_#{@end_date}.csv",
-              type: "text/csv",
-              disposition: "attachment"
-  end
+  #   # Send the CSV data to the browser
+  #   send_data csv_data,
+  #             filename: "profit_loss_statement_#{@start_date}_to_#{@end_date}.csv",
+  #             type: "text/csv",
+  #             disposition: "attachment"
+  # end
 
   private
 
--- backend-ERP/app/models/profit_loss_statement.rb
+++ backend-ERP/app/models/profit_loss_statement.rb
@@ -1,5 +1,3 @@
-require 'csv'
-
 class ProfitLossStatement < ApplicationRecord
   # Define attributes
   attr_accessor :revenue, :expenses, :sales, :accounts_receivable, :accounts_payable
@@ -22,27 +20,4 @@ class ProfitLossStatement < ApplicationRecord
       net_profit_loss: net_profit_loss
     }
   end
-
-  # Method to generate CSV data
-  def generate_csv_data(profit_loss_data)
-    CSV.generate(headers: true) do |csv|
-      csv << ['Sales', 'Revenue', 'Expenses', 'Net Profit/Loss']
-      csv << [profit_loss_data[:sales], profit_loss_data[:revenue], profit_loss_data[:expenses], profit_loss_data[:net_profit_loss]]
-    end
-  end
-
-  # Method to save CSV file
-  def save_csv_file(csv_data)
-    # Specify the file path where the CSV file will be saved
-    file_path = Rails.root.join('public', 'profit_loss_statement.csv')
-  
-    # Write CSV data to the specified file path
-    File.open(file_path, 'w') do |file|
-      file.write(csv_data)
-    end
-  
-    # Return the file path where the CSV file is saved
-    file_path.to_s
-  end
-  
 end
